ae983e1d0f59df1fe05da6fb7b70063b6f1c9c41
Jack_Chiu <57485339+ploki0258@users.noreply.github.com>
2023-05-23 21:50:57 +0800

    Updata 建立角色控制器

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Jack
+{
+    /// <summary>
+    /// 選單管理器
+    /// </summary>
+    public class MenuManager : MonoBehaviour
+    {
+        /// <summary>
+        /// 開始遊戲
+        /// </summary>
+        public void StartGame()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        /// <summary>
+        /// 離開遊戲
+        /// </summary>
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/PiayCtrl.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class PiayCtrl : MonoBehaviour
+{
+    [Header("移動速度")]
+    [SerializeField] float speed = 10;
+
+    Rigidbody2D rig = null;
+    Animator ani = null;
+
+    private void Awake()
+    {
+        // Debug.Log($"{10+20}");
+        
+        rig = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        // Debug.Log("<color=yellow>開始事件</color>");
+    }
+
+    private void Update()
+    {
+        // Debug.Log("<color=red>更新事件</color>");
+        Move();
+    }
+
+    private void Move()
+    {
+        float ad = Input.GetAxisRaw("Horizontal");
+        float ws = Input.GetAxisRaw("Vertical");
+
+        // 角色移動
+        rig.velocity = new Vector2(ad * speed, ws * speed);
+
+        // 移動動畫
+        ani.SetBool("runSwitch", (ws != 0 || ad != 0));
+
+        // 翻轉
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            this.transform.Rotate(0f, 180f, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            this.transform.Rotate(0f, 0f, 0f);
+        }
+    }
+}
